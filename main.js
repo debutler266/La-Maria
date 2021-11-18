4c18c1fb3d55563ecf56ec2d9ba003fd9f84d355
debutler266 <32465329+debutler266@users.noreply.github.com>
2021-11-17 20:07:13 -0500

    added all keyframe animations to each section, & also added the scrollReveal js to each section. All animations complete. Website complete & fully responsive.

--- main.js
+++ main.js
@@ -10,3 +10,34 @@ let body = selectElement('body');
 menuToggler.addEventListener('click', function () {
   body.classList.toggle('open');
 });
+
+
+
+//create instance on scrollReveal
+//using scrollReveal js
+
+//attach to window
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left', {
+  origin: 'left',
+  duration: 1000,
+  distance: '25rem',
+  delay: 300
+});
+
+//target animate-top/bottom elements
+
+sr.reveal('.animate-top', {
+  origin: 'top',
+  duration: 1000,
+  distance: '25rem',
+  delay: 600
+});
+
+sr.reveal('.animate-bottom', {
+  origin: 'bottom',
+  duration: 1000,
+  distance: '25rem',
+  delay: 600
+});
